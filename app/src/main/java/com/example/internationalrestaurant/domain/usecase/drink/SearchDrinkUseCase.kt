0baf3063bc255e04ed95498ec1ba7f19d618f1a2
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-20 23:08:38 +0300

    feat: usecases added :sparkles:

--- app/src/main/java/com/example/internationalrestaurant/domain/usecase/drink/SearchDrinkUseCase.kt
+++ app/src/main/java/com/example/internationalrestaurant/domain/usecase/drink/SearchDrinkUseCase.kt
@@ -1,23 +1,24 @@
-package com.example.internationalrestaurant.domain.usecase
+package com.example.internationalrestaurant.domain.usecase.drink
 
 import android.net.http.HttpException
 import android.os.Build
 import androidx.annotation.RequiresExtension
 import com.example.internationalrestaurant.common.DataStats
-import com.example.internationalrestaurant.data.remote.category.Category
-import com.example.internationalrestaurant.domain.repository.MealRepository
+import com.example.internationalrestaurant.data.remote.drink.toDrink
+import com.example.internationalrestaurant.domain.model.Drink
+import com.example.internationalrestaurant.domain.repository.DrinkRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.io.IOException
 import javax.inject.Inject
-
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
-class GetCategoriesUseCase @Inject constructor(
-    private val mealRepository: MealRepository
+class SearchDrinkUseCase @Inject constructor(
+    private val drinkRepository: DrinkRepository
 ) {
-    operator fun invoke() : Flow<DataStats<List<Category>>> = flow {
+    operator fun invoke(search : String) : Flow<DataStats<List<Drink>>> = flow {
         try {
-            mealRepository.getCategories().categories?.let {
+            emit(DataStats.Loading())
+            drinkRepository.searchDrink(search).drinks?.map { it.toDrink() }?.let {
                 emit(DataStats.Success(it))
             }
         } catch (e : HttpException) {

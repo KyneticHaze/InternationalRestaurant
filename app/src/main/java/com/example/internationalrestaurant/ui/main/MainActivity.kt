630565d7969d6c750b62cd8816b1ae8e25c50ce5
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-21 18:49:22 +0300

    refactor: Build.Version.S requires anotation added :package:

--- app/src/main/java/com/example/internationalrestaurant/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/internationalrestaurant/ui/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.internationalrestaurant.ui.main
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.internationalrestaurant.ui.theme.InternationalRestaurantTheme
 import dagger.hilt.android.AndroidEntryPoint
-
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

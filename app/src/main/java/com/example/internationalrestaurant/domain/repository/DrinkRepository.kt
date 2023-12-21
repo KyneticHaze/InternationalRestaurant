2ba15dd5efefe6debfd2106e9782c679012d782e
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-21 18:50:16 +0300

    refactor: space added :package:

--- app/src/main/java/com/example/internationalrestaurant/domain/repository/DrinkRepository.kt
+++ app/src/main/java/com/example/internationalrestaurant/domain/repository/DrinkRepository.kt
@@ -3,6 +3,5 @@ package com.example.internationalrestaurant.domain.repository
 import com.example.internationalrestaurant.data.remote.drink.DrinkResponse
 
 interface DrinkRepository {
-
     suspend fun searchDrink(search: String) : DrinkResponse
 }

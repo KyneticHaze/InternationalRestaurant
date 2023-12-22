a93771e67761c4ac8216ad9c2d155dd8a21f75c3
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-22 18:22:32 +0300

    feat: query, delete, insert room method added to drink,meal repo interface impl :sparkles:

--- app/src/main/java/com/example/internationalrestaurant/data/repository/DrinkRepositoryImpl.kt
+++ app/src/main/java/com/example/internationalrestaurant/data/repository/DrinkRepositoryImpl.kt
@@ -1,11 +1,25 @@
 package com.example.internationalrestaurant.data.repository
 
+import com.example.internationalrestaurant.data.local.DrinkDao
 import com.example.internationalrestaurant.data.remote.drink.DrinkResponse
 import com.example.internationalrestaurant.data.service.DrinkAPI
+import com.example.internationalrestaurant.domain.model.Drink
 import com.example.internationalrestaurant.domain.repository.DrinkRepository
 
 class DrinkRepositoryImpl(
-    private val drinkAPI: DrinkAPI
+    private val drinkAPI: DrinkAPI,
+    private val drinkDao: DrinkDao
 ) : DrinkRepository {
+
     override suspend fun searchDrink(search: String): DrinkResponse = drinkAPI.searchDrink(search)
+
+    override suspend fun randomDrink(): DrinkResponse = drinkAPI.randomDrink()
+
+    override suspend fun insertDrink(drink: Drink) = drinkDao.insertDrink(drink)
+
+    override suspend fun deleteDrink(drink: Drink) = drinkDao.deleteDrink(drink)
+
+    override suspend fun getDrink(): List<Drink> = drinkDao.getDrinks()
+
+    override suspend fun getDrinkById(id: Int?): Drink? = drinkDao.getMealsById(id)
 }

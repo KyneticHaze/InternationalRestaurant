e48fdb2b64ce643554438d8917e583f70accdf5b
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-22 18:20:15 +0300

    feat: drink and meal daos added to RepositoryModule :sparkles:

--- app/src/main/java/com/example/internationalrestaurant/di/RepositoryModule.kt
+++ app/src/main/java/com/example/internationalrestaurant/di/RepositoryModule.kt
@@ -1,5 +1,8 @@
 package com.example.internationalrestaurant.di
 
+import com.example.internationalrestaurant.data.local.DrinkDao
+import com.example.internationalrestaurant.data.local.MealDao
+import com.example.internationalrestaurant.data.local.RestaurantDB
 import com.example.internationalrestaurant.data.repository.DrinkRepositoryImpl
 import com.example.internationalrestaurant.data.repository.MealRepositoryImpl
 import com.example.internationalrestaurant.data.service.DrinkAPI
@@ -18,9 +21,11 @@ object RepositoryModule {
 
     @Provides
     @Singleton
-    fun provideMealRepository(mealAPI: MealAPI) : MealRepository = MealRepositoryImpl(mealAPI)
+    fun provideMealRepository(mealAPI: MealAPI, db: RestaurantDB): MealRepository =
+        MealRepositoryImpl(mealAPI, db.mealDao())
 
     @Provides
     @Singleton
-    fun provideDrinkRepository(drinkAPI: DrinkAPI) : DrinkRepository = DrinkRepositoryImpl(drinkAPI)
+    fun provideDrinkRepository(drinkAPI: DrinkAPI, db: RestaurantDB): DrinkRepository =
+        DrinkRepositoryImpl(drinkAPI, db.drinkDao())
 }

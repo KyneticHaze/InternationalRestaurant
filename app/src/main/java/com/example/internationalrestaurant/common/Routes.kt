75fc4cce7f3738f92022d460307eb9bd68226c4c
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-22 18:20:38 +0300

    feat: CategoryMeals route added :sparkles:

--- app/src/main/java/com/example/internationalrestaurant/common/Routes.kt
+++ app/src/main/java/com/example/internationalrestaurant/common/Routes.kt
@@ -3,6 +3,7 @@ package com.example.internationalrestaurant.common
 sealed class Routes(val route : String) {
     data object Meal : Routes("meal_screen")
     data object MealDetail : Routes("meal_detail_screen")
+    data object CategoryMeals : Routes("category_meals_screen")
     data object Drink : Routes("drink_screen")
     data object Favorites : Routes("favorites_screen")
 }

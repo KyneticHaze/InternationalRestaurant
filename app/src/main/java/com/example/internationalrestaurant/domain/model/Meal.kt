52ba4e734b04eeb6ad33a7e244d258ff952970fd
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-22 18:23:55 +0300

    refactor: meal model converted room entity :package:

--- app/src/main/java/com/example/internationalrestaurant/domain/model/Meal.kt
+++ app/src/main/java/com/example/internationalrestaurant/domain/model/Meal.kt
@@ -1,11 +1,17 @@
 package com.example.internationalrestaurant.domain.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("meal_table")
 data class Meal(
-    val mealId : String?,
-    val mealName : String?,
-    val mealImg : String?,
-    val mealDesc : String?,
-    val deeplink : String?,
-    val mealCountry : String?,
-    val category : String?
+    @ColumnInfo(name = "meal_id") val mealId : String?,
+    @ColumnInfo(name = "meal_name") val mealName : String?,
+    @ColumnInfo(name = "meal_image") val mealImg : String?,
+    @ColumnInfo(name = "meal_description") val mealDesc : String?,
+    @ColumnInfo(name = "meal_link") val deeplink : String?,
+    @ColumnInfo(name = "meal_country") val mealCountry : String?,
+    @ColumnInfo(name = "mal_category") val category : String?,
+    @PrimaryKey val id : Int? = null
 )

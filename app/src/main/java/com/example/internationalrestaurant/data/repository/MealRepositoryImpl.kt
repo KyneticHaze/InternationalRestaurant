c74884e35dfae423a81c5be63743eb8ba905969d
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-22 18:26:51 +0300

    refactor: mealDao added to MealRepositoryImpl :package:

--- app/src/main/java/com/example/internationalrestaurant/data/repository/MealRepositoryImpl.kt
+++ app/src/main/java/com/example/internationalrestaurant/data/repository/MealRepositoryImpl.kt
@@ -1,12 +1,15 @@
 package com.example.internationalrestaurant.data.repository
 
+import com.example.internationalrestaurant.data.local.MealDao
 import com.example.internationalrestaurant.data.remote.category.CategoriesResponse
 import com.example.internationalrestaurant.data.remote.meal.MealResponse
 import com.example.internationalrestaurant.data.service.MealAPI
+import com.example.internationalrestaurant.domain.model.Meal
 import com.example.internationalrestaurant.domain.repository.MealRepository
 
 class MealRepositoryImpl(
-    private val mealAPI: MealAPI
+    private val mealAPI: MealAPI,
+    private val mealDao : MealDao
 ) : MealRepository {
 
     override suspend fun getCategories(): CategoriesResponse = mealAPI.getCategories()
@@ -14,5 +17,14 @@ class MealRepositoryImpl(
     override suspend fun allMealsFirstLetter(firstLetter: String): MealResponse = mealAPI.allMealsFirstLetter(firstLetter)
 
     override suspend fun mealDetail(mealId: String): MealResponse = mealAPI.mealDetail(mealId)
+
     override suspend fun filterCategories(filterCategory: String): MealResponse = mealAPI.filterCategories(filterCategory)
+
+    override suspend fun insertMeal(meal: Meal) = mealDao.insertMeal(meal)
+
+    override suspend fun deleteMeal(meal: Meal) = mealDao.deleteMeal(meal)
+
+    override suspend fun getMeals(): List<Meal> = mealDao.getMeals()
+
+    override suspend fun getMealById(id: Int?): Meal? = mealDao.getMealsById(id)
 }

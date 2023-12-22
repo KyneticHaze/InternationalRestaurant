ebce1023e9221512815af75a3568c5ae22771806
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-22 18:24:45 +0300

    feat: Icon added to MealCard comps :sparkles:

--- app/src/main/java/com/example/internationalrestaurant/ui/screens/meal/meals/components/MealCard.kt
+++ app/src/main/java/com/example/internationalrestaurant/ui/screens/meal/meals/components/MealCard.kt
@@ -6,8 +6,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,11 +28,13 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.internationalrestaurant.domain.model.Meal
 import com.example.internationalrestaurant.ui.theme.background
+import com.example.internationalrestaurant.ui.theme.primary
 
 @Composable
 fun MealCard(
     meal: Meal,
-    onMealClick : (String) -> Unit
+    onMealClick : (String) -> Unit,
+    starredMeal : (Meal) -> Unit
 ) {
     Card(
         elevation = CardDefaults.cardElevation(
@@ -37,13 +44,31 @@ fun MealCard(
             .padding(10.dp)
             .clickable { onMealClick(meal.mealId.orEmpty()) }
     ) {
-        Box {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
             AsyncImage(
                 model = meal.mealImg,
                 contentDescription = meal.mealName,
                 contentScale = ContentScale.Fit,
                 modifier = Modifier.fillMaxWidth()
             )
+            IconButton(
+                onClick = {
+                    starredMeal(meal)
+                }, modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .background(
+                        primary, shape = RoundedCornerShape(10.dp)
+                    )
+                    .size(35.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Star,
+                    contentDescription = "Favorite Drink",
+                    tint = background
+                )
+            }
             Text(
                 text = meal.mealName.orEmpty(),
                 fontSize = 16.sp,

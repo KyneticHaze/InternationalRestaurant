da7d24a0ea5ede64dc53b0897d2072c69da790a7
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-22 18:22:21 +0300

    feat: query, delete, insert room method added to drink,meal repo interface :sparkles:

--- app/src/main/java/com/example/internationalrestaurant/domain/repository/DrinkRepository.kt
+++ app/src/main/java/com/example/internationalrestaurant/domain/repository/DrinkRepository.kt
@@ -1,7 +1,18 @@
 package com.example.internationalrestaurant.domain.repository
 
 import com.example.internationalrestaurant.data.remote.drink.DrinkResponse
+import com.example.internationalrestaurant.domain.model.Drink
 
 interface DrinkRepository {
     suspend fun searchDrink(search: String) : DrinkResponse
+
+    suspend fun randomDrink() : DrinkResponse
+
+    suspend fun insertDrink(drink: Drink)
+
+    suspend fun deleteDrink(drink: Drink)
+
+    suspend fun getDrink() : List<Drink>
+
+    suspend fun getDrinkById(id : Int?) : Drink?
 }
--- app/src/main/java/com/example/internationalrestaurant/domain/repository/MealRepository.kt
+++ app/src/main/java/com/example/internationalrestaurant/domain/repository/MealRepository.kt
@@ -2,6 +2,7 @@ package com.example.internationalrestaurant.domain.repository
 
 import com.example.internationalrestaurant.data.remote.category.CategoriesResponse
 import com.example.internationalrestaurant.data.remote.meal.MealResponse
+import com.example.internationalrestaurant.domain.model.Meal
 
 interface MealRepository {
 
@@ -12,4 +13,12 @@ interface MealRepository {
     suspend fun mealDetail(mealId: String) : MealResponse
 
     suspend fun filterCategories(filterCategory: String) : MealResponse
+
+    suspend fun insertMeal(meal : Meal)
+
+    suspend fun deleteMeal(meal : Meal)
+
+    suspend fun getMeals() : List<Meal>
+
+    suspend fun getMealById(id : Int?) : Meal?
 }

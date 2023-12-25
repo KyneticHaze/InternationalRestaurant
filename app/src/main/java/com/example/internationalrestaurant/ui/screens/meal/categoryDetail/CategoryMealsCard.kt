f4772873e8f79f60113ed586c6f7df651aeea97a
Furkan Harmancı <rocklee34@hotmail.com>
2023-12-26 00:06:06 +0300

    feat: Category components added :sparkles:

---
+++ app/src/main/java/com/example/internationalrestaurant/ui/screens/meal/categoryDetail/CategoryMealsCard.kt
@@ -0,0 +1,90 @@
+package com.example.internationalrestaurant.ui.screens.meal.categoryDetail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.internationalrestaurant.domain.model.Meal
+import com.example.internationalrestaurant.ui.theme.background
+import com.example.internationalrestaurant.ui.theme.primary
+
+@Composable
+fun CategoryMealsCard(
+    meal: Meal,
+    starredMeal: (Meal) -> Unit
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        ),
+        modifier = Modifier
+            .padding(10.dp)
+    ) {
+        Box {
+            AsyncImage(
+                model = meal.mealImg,
+                contentDescription = meal.mealName,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.fillMaxWidth()
+            )
+            IconButton(
+                onClick = {
+                    starredMeal(meal)
+                }, modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .background(
+                        primary, shape = RoundedCornerShape(10.dp)
+                    )
+                    .size(35.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Star,
+                    contentDescription = "Favorite Drink",
+                    tint = background
+                )
+            }
+            Text(
+                text = meal.mealName.orEmpty(),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.W500,
+                fontStyle = FontStyle.Italic,
+                color = background,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                Color.Transparent,
+                                Color.Gray
+                            )
+                        )
+                    )
+                    .padding(8.dp)
+            )
+        }
+    }
+}
+
